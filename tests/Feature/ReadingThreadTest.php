54c1b59a661faade8dde04dfa28c72cfd49adbfd
Sampuccino <Sampuccino@gmail.com>
2019-07-09 23:20:17 -0700

    Updated testing XML file to use sqlite table and prevent dropping all table data when testing. Also fixed slight issue when testing and only asserting / using 1 test when there was more.

---
+++ tests/Feature/ReadingThreadTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Reply;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ReadingThreadTest extends TestCase
+{
+
+  use DatabaseMigrations;
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    protected function setUp(): void
+    {
+      parent::setUp();
+      $this->thread = $thread = factory('App\Thread')->create();
+    }
+
+  /**
+   * @test
+   */
+    public function a_user_can_view_all_threads()
+    {
+      $response = $this->get('/threads')
+        ->assertSee($this->thread->title);
+    }
+
+    /**
+     * @test
+     */
+    public function a_user_can_read_a_single_thread(){
+      $this->get('/threads/' . $this->thread->id)
+        ->assertSee($this->thread->title);
+    }
+
+    /**
+     * @test
+     */
+//    function a_user_can_read_replies_that_are_associated_with_thread() {
+//      /* Generate 1 reply */
+//      $reply = factory(Reply::class)
+//        ->create(['thread_id' => $this->thread->id]);
+//
+//      $this->get('/threads/'. $this->thread->id)
+//        ->assertSee($reply->body);
+//    }
+
+}
